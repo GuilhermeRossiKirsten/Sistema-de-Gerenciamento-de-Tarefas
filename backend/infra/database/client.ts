8a2d4b3b99d01ac9974a866e50d2ad58aec27ed2
Guilherme Rossi Kirsten <guirossik10@gmail.com>
2025-10-01 10:55:38 -0300

    fix: ajuste ssl connection

--- backend/infra/database/client.ts
+++ backend/infra/database/client.ts
@@ -1,5 +1,12 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import pkg from "pg";
+const { Pool } = pkg;
 
-export const db = drizzle(process.env.DATABASE_URL, {
-  logger: true,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // permite SSL sem verificar certificado
+  },
 });
+
+export const db = drizzle(pool, { logger: true });
